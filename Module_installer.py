34ec026d5e97d1d9a238624abb2a84ca6f3f8524
linuxzaman <linux.zaman@gmail.com>
2019-06-25 20:00:28 +0530

    Added date and time

--- Module_installer.py
+++ Module_installer.py
@@ -1,18 +1,17 @@
+##Python program to check weather packages are installed or not
+#date and time : 7:59 PM 25-Jun-19
 def module_check():
     try:
         import subprocess
         import requests as pd
-        print("I am BACK:::::::::::::")
         print(pd)
-        print(1/0)
+        
     except ImportError as e:
         import os
         mod = str(e)
         mod = list(mod.split())
         cmd = 'pip3 install '+mod[-1]+';'+'pip install '+mod[-1]
-        print(cmd)
         os.system(cmd)
-        print("Hello world again")
         module_check()
         
 module_check()
